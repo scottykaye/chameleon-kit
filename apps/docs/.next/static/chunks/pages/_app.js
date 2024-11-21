2b15c749a248170cf200f07ab2dd7a8e9ef82dbe
Scott Kaye <scottykaye.web@gmail.com>
2024-11-21 16:51:07 -0500

    remove .nexts

--- apps/docs/.next/static/chunks/pages/_app.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/[root of the server]__14669f._.js",
-  "static/chunks/08b5e_react-dom_82bb97._.js",
-  "static/chunks/08b5e_beb007._.js",
-  "static/chunks/[root of the server]__70a901._.js",
-  "static/chunks/pages__app_5771e1._.js",
-  "static/chunks/pages__app_eab5d8._.js"
-])
